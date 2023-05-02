acab7bc445d7dbe4adb93b7a325fd43d86c4e49d
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-05-02 19:04:52 +0200

    Added

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const container = document.querySelector(".container");
+const divContainer = document.querySelector(".container");
+
+const div = document.createElement("div");
+
+for (let i = 0; i < 256; i++) {
+  let div = document.createElement("div");
+  div.addEventListener("mouseover", () => {
+    div.setAttribute("style", "background-color: black");
+  });
+  divContainer.appendChild(div);
+}
