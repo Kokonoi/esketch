942083f2cfd766274008e5c9f1e94ed246f4b485
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-05-02 20:18:10 +0200

    Added functions to set res

--- main.js
+++ main.js
@@ -2,10 +2,33 @@ const divContainer = document.querySelector(".container");
 
 const div = document.createElement("div");
 
-for (let i = 0; i < 256; i++) {
-  let div = document.createElement("div");
-  div.addEventListener("mouseover", () => {
-    div.setAttribute("style", "background-color: black");
-  });
-  divContainer.appendChild(div);
+setDefault();
+
+function setDefault() {
+  for (let i = 0; i < 256; i++) {
+    let div = document.createElement("div");
+    div.addEventListener("mouseover", () => {
+      div.setAttribute("style", "background-color: black");
+    });
+    divContainer.appendChild(div);
+  }
+}
+function setSixteen() {
+  for (let i = 0; i < 64; i++) {
+    let div = document.createElement("div");
+    div.addEventListener("mouseover", () => {
+      div.setAttribute("style", "background-color: black");
+    });
+    divContainer.appendChild(div);
+  }
+}
+
+function setThirtytwo() {
+  for (let i = 0; i < 1024; i++) {
+    let div = document.createElement("div");
+    div.addEventListener("mouseover", () => {
+      div.setAttribute("style", "background-color: black");
+    });
+    divContainer.appendChild(div);
+  }
 }
