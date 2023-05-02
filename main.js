7bf88145673573fda54e3edf31640811d1d19900
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-05-02 21:14:06 +0200

    Made a working res

--- main.js
+++ main.js
@@ -7,10 +7,9 @@ const blackButton = document.querySelector(".btn4");
 const rbgButton = document.querySelector(".btn5");
 const clearButton = document.querySelector(".btn6");
 
-setDefault();
-
 function setDefault() {
   // 16x16=265 => i must be defined this way so the squares take all the space
+  divContainer.replaceChildren();
   for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
     div.addEventListener("mouseover", () => {
@@ -20,22 +19,53 @@ function setDefault() {
   }
 }
 
-function setSixteen() {
+function setEight() {
+  divContainer.replaceChildren();
   for (let i = 0; i < 64; i++) {
     let div = document.createElement("div");
+    div.setAttribute("style", "width:50px; height:50px");
     div.addEventListener("mouseover", () => {
-      div.setAttribute("style", "background-color: black");
+      div.setAttribute(
+        "style",
+        "background-color: black; width:50px; height:50px"
+      );
     });
     divContainer.appendChild(div);
   }
 }
 
 function setThirtytwo() {
+  divContainer.replaceChildren();
   for (let i = 0; i < 1024; i++) {
     let div = document.createElement("div");
+    div.setAttribute("style", "width:12.5px; height:12.5px");
     div.addEventListener("mouseover", () => {
-      div.setAttribute("style", "background-color: black");
+      div.setAttribute(
+        "style",
+        "background-color: black;width:12.5px; height:12.5px;"
+      );
     });
     divContainer.appendChild(div);
   }
 }
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+firstButton.addEventListener("click", () => {
+  setEight();
+});
+
+secondButton.addEventListener("click", () => {
+  setDefault();
+});
+
+thirdButton.addEventListener("click", () => {
+  setThirtytwo();
+});
