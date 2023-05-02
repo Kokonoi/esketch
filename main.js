5bdb991cf5ddfcf0e3214b68fb8c91abfeee69bb
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-05-02 20:25:38 +0200

    Added comments

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const divContainer = document.querySelector(".container");
 const div = document.createElement("div");
 const firstButton = document.querySelector(".btn1");
 const secondButton = document.querySelector(".btn2");
-
 const thirdButton = document.querySelector(".btn3");
 const blackButton = document.querySelector(".btn4");
 const rbgButton = document.querySelector(".btn5");
@@ -11,6 +10,7 @@ const clearButton = document.querySelector(".btn6");
 setDefault();
 
 function setDefault() {
+  // 16x16=265 => i must be defined this way so the squares take all the space
   for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
     div.addEventListener("mouseover", () => {
